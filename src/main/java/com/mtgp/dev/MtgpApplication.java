bea8a835b0c71d62a18f38fe4f23cf330d4dd51e
Garry Cabrera <cabrera82@gmail.com>
2018-04-02 21:33:27 -0400

    Initial commit with barebone structure

---
+++ src/main/java/com/mtgp/dev/MtgpApplication.java
@@ -0,0 +1,12 @@
+package com.mtgp.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtgpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtgpApplication.class, args);
+	}
+}
